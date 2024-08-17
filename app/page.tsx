ed2e445b3bc7be8b4d167a8d5f666895d432a3d3
Chareeef <youssef.charif.h@gmail.com>
2024-08-17 22:32:33 +0000

    Animate landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function LandingPage() {
   const features = [
@@ -21,8 +25,14 @@ export default function LandingPage() {
     },
   ];
 
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // Animation duration in milliseconds
+    });
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-around gap-y-4 p-4 bg-tertiary text-center text-black">
+    <div className="flex flex-col items-center justify-around overflow-hidden gap-y-4 p-4 bg-tertiary text-center text-black">
       <div className="flex flex-col items-center gap-y-2">
         <Image
           src="/icons/icon.png"
@@ -30,24 +40,32 @@ export default function LandingPage() {
           height={510}
           width={510}
           className="h-[3rem] w-[3rem] rounded-xl shadow-inner"
+          data-aos="fade-left"
         />
 
-        <h1 className="text-lg font-bold">AI Flashcards</h1>
+        <h1 className="text-lg font-bold" data-aos="fade-right">
+          AI Flashcards
+        </h1>
       </div>
 
-      <div className="border-2 border-indigo-500 grid grid-cols-2 gap-0 w-full">
+      <ul className="flex flex-col gap-0 w-full" data-aos="zoom-out-up">
         {features.map((feature, index) => (
-          <>
+          <li className="w-full grid grid-cols-2 gap-0" key={index}>
             {index % 2 !== 0 && (
               <Image
                 src={feature.imageURL}
                 alt={feature.text}
                 height={571}
                 width={960}
+                data-aos="zoom-out-right"
+                className="border-y-2 border-l-2 border-r border-indigo-500"
               />
             )}
 
-            <div className="shadow-inner bg-gradient-to-r from-indigo-300 to-violet-300 p-2 flex items-center justify-center">
+            <div
+              className={`${index % 2 === 0 ? "border-l-2" : "border-r-2"} border-y-2 border-indigo-500 shadow-inner bg-gradient-to-r from-indigo-300 to-violet-300 p-2 flex items-center justify-center`}
+              data-aos="zoom-in"
+            >
               {feature.text}
             </div>
 
@@ -57,14 +75,16 @@ export default function LandingPage() {
                 alt={feature.text}
                 height={571}
                 width={960}
+                className="border-y-2 border-r-2 border-l border-indigo-500"
+                data-aos="zoom-out-left"
               />
             )}
-          </>
+          </li>
         ))}
-      </div>
+      </ul>
 
-      <Link href="/sign-up" className="btn-cta">
-        Take your reviews to the next level!
+      <Link href="/sign-up" className="btn-cta my-4" data-aos="zoom-in-right">
+        Take Your Reviews To The Next Level !
       </Link>
     </div>
   );
